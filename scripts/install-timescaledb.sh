215863d0bbb2364814c9fced5b4e5c63feb20423
Matt Bishop <matt.bishop@conexon.us>
2025-10-20 10:50:30 -0500

    update timescale install script

--- scripts/install-timescaledb.sh
+++ scripts/install-timescaledb.sh
@@ -5,31 +5,31 @@
 set -euo pipefail
 
 success() {
-        local GREEN='\033[32m'
-        local RESET='\033[0m'
-        echo -e "${GREEN}$1${RESET}"
+  local GREEN='\033[32m'
+  local RESET='\033[0m'
+  echo -e "${GREEN}$1${RESET}"
 }
 
 error() {
-        local RED='\033[31m'
-        local RESET='\033[0m'
-        echo -e "${RED}$1${RESET}"
+  local RED='\033[31m'
+  local RESET='\033[0m'
+  echo -e "${RED}$1${RESET}"
 }
 
 echo "Ensuring wget and gpg are present.."
 
-apt-get -y install wget gnupg2 > /dev/null
+apt-get -y install wget gnupg2 >/dev/null
 
 if [[ -f /etc/apt/sources.list.d/timescaledb.list ]]; then
-        echo "TimescaleDB package already added, skipping.."
+  echo "TimescaleDB package already added, skipping.."
 else
-        echo "deb https://packagecloud.io/timescale/timescaledb/debian/ $(lsb_release -c -s) main" | tee /etc/apt/sources.list.d/timescaledb.list
+  echo "deb https://packagecloud.io/timescale/timescaledb/debian/ $(lsb_release -c -s) main" | tee /etc/apt/sources.list.d/timescaledb.list
 fi
 
 if [[ -f /etc/apt/trusted.gpg.d/timescaledb.gpg ]]; then
-        echo "Timescale GPG key already added, skipping.."
+  echo "Timescale GPG key already added, skipping.."
 else
-        wget --quiet -O - https://packagecloud.io/timescale/timescaledb/gpgkey | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/timescaledb.gpg
+  wget --quiet -O - https://packagecloud.io/timescale/timescaledb/gpgkey | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/timescaledb.gpg
 fi
 
 echo "Updating packages.."
@@ -38,14 +38,16 @@ apt-get update
 
 read -p "What version of PostgreSQL do you want to install Timescale for? " pg_version
 
+read -p "What version of TimescaleDB do you want to install?" timescale_version
+
 if [[ -z "$pg_version" || ! "$pg_version" =~ ^[0-9]+$ ]]; then
-        error "PostgreSQL number must not be empty and must be a number (e.g. 16). Exiting."
-        exit 1
+  error "PostgreSQL number must not be empty and must be a number (e.g. 16). Exiting."
+  exit 1
 fi
 
-echo "Installing timescaledb-2-postgresql-$pg_version"
+echo "Installing timescaledb-2-postgresql-$pg_version='$timescale_version'"
 
-apt-get install -y timescaledb-2-postgresql-"$pg_version" > /dev/null
+apt-get install -y timescaledb-2-postgresql-"$pg_version='$timescale_version'" >/dev/null
 
 echo "Running timescaledb-tune"
 
@@ -58,3 +60,4 @@ echo "Restarting postgresql.."
 systemctl restart postgresql
 
 success "Done. TimescaleDB installed."
+
